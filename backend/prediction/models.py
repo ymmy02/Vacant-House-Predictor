3849a69928ab4fdb3b571955bd0b07f4e49f3d25
Kohei Yamamoto <kh2yam1@gmial.com>
2019-06-22 19:47:28 -0400

    Implement API

--- backend/prediction/models.py
+++ backend/prediction/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class HouseImage(models.Model):
+    image = models.ImageField(blank=False, null=False)
+    def __str__(self):
+        return self.image.name
--- backend/prediction/views.py
+++ backend/prediction/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from PIL import Image
+from .serializers import HouseImageSerializer
+from .predictor import predict
+
+class ImageUploadParser(FileUploadParser):
+    media_type = 'image/*'
+
+class PredictionView(APIView):
+    parser_class = (ImageUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+        house_image_serializer = HouseImageSerializer(data=request.data)
+        # house_image_serializer.save()
+        if house_image_serializer.is_valid():
+            pil_image = Image.open(request.data['image']).convert('RGB')
+            occupied_pct, vacant_pct = predict(pil_image)
+            return Response({"occupied": occupied_pct, "vacant": vacant_pct})
+        else:
+            return Response(house_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
