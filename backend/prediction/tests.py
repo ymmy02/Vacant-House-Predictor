071cd36413ab2c50beb49db177664b30c550f667
Kohei Yamamoto <kh2yam1@gmial.com>
2019-06-21 22:21:40 -0400

    Add first red test case

--- backend/prediction/tests.py
+++ backend/prediction/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .predictor import predict
 
 # Create your tests here.
+class PredictorFunctionTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_is_parcentage(self):
+        house_image = object
+        occupied_pct, vacant_pct = predict(house_image)
+        self.assertTrue(0 <= occupied_pct <= 100)
+        self.assertTrue(0 <= vacant_pct <= 100)
