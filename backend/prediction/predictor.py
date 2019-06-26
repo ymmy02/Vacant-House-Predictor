db57f01803ac882305ce41e8150c9f8611e817d5
Kohei Yamamoto <kh2yam1@gmial.com>
2019-06-26 18:06:10 -0400

    Modify the problem which occurs after some requests

--- backend/prediction/predictor.py
+++ backend/prediction/predictor.py
@@ -2,6 +2,7 @@ from typing import Tuple
 from django.conf import settings
 import PIL
 import numpy as np
+from keras import backend as K
 from keras.models import load_model
 
 def predict(house_image: PIL.Image.Image) -> Tuple[float, float]:
@@ -19,7 +20,7 @@ def convert_pilimage_to_nparray(pil_image: PIL.Image.Image) -> np.array:
 def predict_by_cnn_keras(np_image) -> Tuple[float, float]:
     model = load_model(settings.MLMODEL_ROOT + '/cnn_keras.h5')
     vacant_rate = model.predict_proba(np_image)[0][0]
+    K.clear_session()
     vacant_pct = vacant_rate * 100
     occupied_pct = 100 - vacant_pct
     return occupied_pct, vacant_pct
-
