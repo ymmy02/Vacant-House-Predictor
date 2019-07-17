86ea4c6eae54e75ea95f8fa3c5f53290e11d47cb
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-17 12:40:28 -0400

    Modify cors origin whitelist

--- vacant_house_predictor/backend/settings.py
+++ vacant_house_predictor/backend/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'https://localhost:3000',
+    'http://localhost:8080',
+    'https://localhost:8080',
 )
 
 ROOT_URLCONF = 'backend.urls'
