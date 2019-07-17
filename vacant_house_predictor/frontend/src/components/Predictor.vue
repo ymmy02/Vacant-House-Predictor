3ec695fd7069766091a61d28e600a313ffa54fb4
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-17 12:34:48 -0400

    Implement frontend

--- vacant_house_predictor/frontend/src/components/Predictor.vue
+++ vacant_house_predictor/frontend/src/components/Predictor.vue
@@ -14,7 +14,21 @@
             住宅の外観画像を送信してください。
             </b-card-text>
 
-            <b-button href="#" variant="outline-primary">Go somewhere</b-button>
+            <b-form-file 
+                accept="image/*"
+                :file-name-formatter="formatNames"
+                @change="changeImage"></b-form-file>
+
+            <b-button variant="outline-primary" @click="submitImage">送信</b-button>
+            <div class="d-flex justify-content-center mb-3" v-if="loading">
+                <b-spinner label="Loading..."></b-spinner>
+            </div>
+            <b-alert
+                :show="showResult"
+                dismissible
+                :variant="alertType"
+                @dismissed="showResult=false"
+            >{{ resultMessage }}</b-alert>
         </b-card>
     </div>
     </div>
@@ -22,13 +36,67 @@
 
 <script>
 import noimage from "../assets/noimage.png";
+import { apiUrl } from "../constants";
+import axios from 'axios';
 
 export default {
     data() {
         return {
+            image: null,
             imageUrl: noimage,
+            loading: false,
+            showResult: false,
+            resultMessage: '',
+            alertType: ''
         }
+    },
+    methods: {
+      formatNames(files) {
+        return files[0].name
+      },
+      changeImage(e) {
+        this.showResult = false,
+        this.resultMessage = '';
+        this.alertType = '';
+        this.image = e.target.files[0];
+        this.imageUrl = this.image ? URL.createObjectURL(this.image) : noimage;
+      },
+      submitImage(e) {
+        this.loading = true;
+        const formData = new FormData();
+        formData.append('image', this.image)
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+        console.log(apiUrl);
+        axios.post(apiUrl, formData, config)
+            .then(res => {
+                const occupied = res.data.occupied;
+                const vacant = res.data.vacant;
+                console.log(`Occupied Parcentage: ${occupied}`);
+                console.log(`Vacant Parcentage: ${vacant}`);
+
+                this.judgeAndShowResult(occupied, vacant);
+            })
+            .catch(err => console.log(err))
+            .finally(() => this.loading = false);
+      },
+      judgeAndShowResult(occupied, vacant) {
+        if (occupied > vacant) {
+            this.resultMessage = "空き家でないと判断されました。";
+            this.alertType = 'info';
+        } else {
+            this.resultMessage = "空き家と判断されました。"
+            this.alertType = 'danger';
+        }
+        this.showResult = true;
+      }
     }
 }
 </script>
+
+<style scoped>
+</style>
 
---
+++ vacant_house_predictor/frontend/src/constants.js
@@ -0,0 +1,1 @@
+export const apiUrl = process.env.NODE_ENV === "production" ? 'http://vacant.house.predictor:8000/api/' : 'http://localhost:8000/api/';
